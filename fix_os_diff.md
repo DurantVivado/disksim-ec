6e7c3be7365b37416dd90eb6a8054403dbcd1362
Une histoire lament <30594129+DurantVivado@users.noreply.github.com>
2021-09-06 20:47:29 +0800

    first version in public

---
+++ fix_os_diff.md
@@ -0,0 +1,5 @@
+If you use Ubuntu or other OS that is different than Ubuntu 16.04, you should delete all `*.d` files(dependency files) before make.
+
+or use shell script:
+	sh ./fix_os_diff.sh
+Good Luck!
---
+++ fix_os_diff.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env
+
+cd src/
+echo "Enter src"
+sudo rm *.d
+echo "Enter memsmodel"
+cd ../memsmodel
+sudo rm *.d
+echo "Enter diskmodel"
+cd ../diskmodel
+sudo rm *.d
+echo ".d deleted done"
+cd ../
